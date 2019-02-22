95927433049a885a66dd77f511197bbfd683ac9c
Holden Beale <itdbealh@aikencounty.net>
2019-02-22 08:50:07 -0500

    can't remember

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "./components/SignIn.js"
+
+class App extends React.Component {
+  render() {
+    return (
+      <SignIn />
+    )
+  }
+}
+export default App;
